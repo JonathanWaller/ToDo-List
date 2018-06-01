0594ea8dda167a27dba644ebce584541d14b9b10
JonathanWaller <waller.j27@gmail.com>
2018-06-01 14:11:31 -0500

    complete, no style

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: "",
+      list: ["mow lawn", "empty trash", "walk dog"]
+    };
+  }
+
+  handleChange(val) {
+    this.setState({
+      userInput: val
+    });
+  }
+
+  handleClick(userInput) {
+    let listCopy = this.state.list.slice();
+    listCopy.push(this.state.userInput);
+    this.setState({
+      list: listCopy
+    });
+  }
+
+  deleteHandler(i) {
+    let deleteItem = this.state.list.slice();
+    deleteItem.splice(i, 1);
+    this.setState({ list: deleteItem });
+  }
+
+  render() {
+    let myList = this.state.list.map((elem, ind) => {
+      return (
+        <h3 onClick={() => this.deleteHandler(ind)} key={ind}>
+          {elem}
+        </h3>
+      );
+    });
+    return (
+      <div className="App">
+        <h1>ToDo List</h1>
+        <input
+          onChange={e => {
+            this.handleChange(e.target.value);
+          }}
+          placeholder="add items"
+        />
+        {/* <p>{this.state.userInput}</p> */}
+        <button onClick={() => this.handleClick()}>Submit</button>
+        {/* <p>{this.state.list}</p> */}
+        {myList}
+      </div>
+    );
+  }
+}
+
+export default App;
